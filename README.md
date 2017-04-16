60edc4158c11fb7c6a9c5d2f0b742f882a6f83fd
Mark Wilson <mark@warkmilson.com>
2017-04-16 16:13:38 -0700

    Scrape Nightscout reports instead

--- README.md
+++ README.md
@@ -6,14 +6,10 @@
 
 1. Make a file called `.env` like
     ```
-    CLARITY_USERNAME="username"
-    CLARITY_PASSWORD="password"
+    NIGHTSCOUT_HOST="https://your.nightscout.host"
+
     # Find this URL under "Environment variable" in the "Account Details" section of the "Dashboard" page
     CLOUDINARY_URL="cloudinary://<api_key>:<api_secret>@<cloud_name>"
     ```
 
-1. `node run.js | mail --subject "Clarity weekly summary" --append "Content-type:text/html" --append "From:from_address@domain.com" to_address@domain.com`
-
-## To-do
-
-* Specify date range and intelligently segment that into 5-day chunks, instead of hard-coding the last week
+1. `node run.js | mail --subject "Nightscout weekly summary" --append "Content-type:text/html" --append "From:from_address@domain.com" to_address@domain.com`
--- run.js
+++ run.js
@@ -3,20 +3,16 @@ require('dotenv').config()
 var cloudinary = require('cloudinary');
 var fetch = require('node-fetch');
 var fs = require('fs');
+var moment = require('moment');
 var Nightmare = require('nightmare');
 
 var WAIT = 5000;
 var SHOW = false;
 
 var endDate = new Date();
-// Clarity shows only 5 daily charts per page
-var firstPeriod = [
-  new Date(endDate - 5 * 24 * 60 * 60 * 1000).toISOString().substr(0, 10),
-  endDate.toISOString().substr(0, 10),
-];
-var secondPeriod = [
-  new Date(endDate - 7 * 24 * 60 * 60 * 1000).toISOString().substr(0, 10),
-  new Date(endDate - 5 * 24 * 60 * 60 * 1000).toISOString().substr(0, 10),
+var dateRangeStrings = [
+  moment(endDate).subtract(7, 'days').format('MM/DD/YYYY'),
+  moment(endDate).format('MM/DD/YYYY'),
 ];
 
 var xvfb;
@@ -39,18 +35,16 @@ startFb();
 
 var nightmare = Nightmare({show: SHOW});
 nightmare
-  .goto('https://clarity.dexcom.com/')
-  .click('a[href="/users/auth/dexcom_sts"]')
+  .goto(process.env.NIGHTSCOUT_HOST + '/report/index.html')
   .wait(WAIT)
-  .insert('input[name=username]', process.env.CLARITY_USERNAME)
-  .insert('input[name=password]', process.env.CLARITY_PASSWORD)
-  .click('input[type=submit]')
-  .wait(WAIT)
-  .goto('https://clarity.dexcom.com/#/data/daily?dates=' + firstPeriod[0] + '%2F' + firstPeriod[1])
-  .wait(WAIT)
+  .type('#rp_from', dateRangeStrings[0])
+  .type('#rp_to', dateRangeStrings[1])
+  .uncheck('#rp_optionsnotes')
+  .click('#rp_show')
+  .wait(WAIT * 2)
   .evaluate(function() {
     var charts = Array.prototype.map.call(
-      document.querySelectorAll('daily-strip-chart'),
+      document.querySelectorAll('#daytodaycharts > table'),
       function(el) { return el.outerHTML; }
     );
     var cssUrls = Array.prototype.map.call(
@@ -58,34 +52,22 @@ nightmare
       function(el) { return el.href; }
     )
     return [charts, cssUrls];
-  })
-  .then(function(chartsAndCssUrls) {
-    nightmare
-      .goto('https://clarity.dexcom.com/#/data/daily?dates=' + secondPeriod[0] + '%2F' + secondPeriod[1])
-      .wait(WAIT)
-      .evaluate(function() {
-        return Array.prototype.map.call(
-          document.querySelectorAll('daily-strip-chart'),
-          function(el) { return el.outerHTML; }
-        );
+  }).then(function(chartsAndCssUrls) {
+    nightmare.end().then(function() {});
+
+    var charts = chartsAndCssUrls[0];
+    var cssUrls = chartsAndCssUrls[1];
+
+    var cssFetches = cssUrls.map(function(url) {
+      return fetch(url).then(function(res) { return res.text(); })
+    });
+
+    Promise.all(cssFetches)
+      .then(function(csses) {
+        return makeChartPngs(charts, csses.join('\n'));
       })
-      .then(function(pageTwoCharts) {
-        nightmare.end().then(function() {});
-
-        var allCharts = chartsAndCssUrls[0].concat(pageTwoCharts);
-        var cssUrls = chartsAndCssUrls[1];
-
-        var cssFetches = cssUrls.map(function(url) {
-          return fetch(url).then(function(res) { return res.text(); })
-        });
-
-        Promise.all(cssFetches)
-          .then(function(csses) {
-            return makeChartPngs(allCharts, csses.join('\n'));
-          })
-          .then(uploadChartPngs)
-          .then(makeHtml);
-      })
+      .then(uploadChartPngs)
+      .then(makeHtml);
   })
 
 function makeChartPngs(charts, cssContent) {
@@ -100,7 +82,7 @@ function makeChartPngs(charts, cssContent) {
     ].join('');
     var screenshotFile = '/tmp/chart-' + i + '.png';
     return Nightmare({show: SHOW})
-      .viewport(900, 290)
+      .viewport(1200, 450)
       .goto('data:text/html,' + html)
       .screenshot(screenshotFile)
       .end()
